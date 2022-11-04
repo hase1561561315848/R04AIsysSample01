9534fb0e82e381976b4d52d25f2dbed5fc486ea8
hase1561561315848 <115970716+hase1561561315848@users.noreply.github.com>
2022-11-04 12:22:25 +0900

    Update Json05.java
    proxyのありなし両方に対応（10月31日、11月10日）

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
@@ -35,10 +35,16 @@ public class Json05 {
 
 		String jsonData = new Gson().toJson(src);
 
-		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
+		JsonReader reader = null;
+		try {
+			InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
 
-		JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
-// 		JsonReader reader = WebApiConnector.postJsonReader(url, map, jsonData);
+			reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
+		}catch(Exception e) {
+			reader = WebApiConnector.postJsonReader(url, map, jsonData);
+			
+		}
+		
 		Language message = null;
 		if (reader != null) {
 			message = gson.fromJson(reader, Language.class);
